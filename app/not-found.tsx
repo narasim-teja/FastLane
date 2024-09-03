48fbd674be16fdaab8c17e679d8466a62054d786
rajput-hemant <rajput.hemant2001@gmail.com>
2024-09-03 17:36:58 +0530

    refactor: revise 404 -  Not Found Page
    Signed-off-by: rajput-hemant <rajput.hemant2001@gmail.com>

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,32 +1,42 @@
-import Link from "next/link";
+import Image from "next/image";
 
-import { Gamepad, MoveLeft } from "lucide-react";
-
-import { Separator } from "~/components/ui/separator";
-import { siteConfig } from "~/config/site";
+import { Navbar } from "~/components/site-header/navbar";
+import { Button } from "~/components/ui/button";
 
 export default function NotFoundPage() {
   return (
-    <div className="grid h-screen place-items-center">
-      <div className="space-y-4 rounded-4xl border-dashed bg-white p-4 sm:border md:p-8 md:py-12">
-        <p className="font-cal inline-flex items-center gap-2 text-4xl lowercase">
-          <Gamepad className="size-10" /> {siteConfig.name}
-        </p>
-        <Separator />
+    <div className="relative z-0 grid h-screen place-items-center">
+      <Navbar />
 
-        <h1 className="font-cal text-3xl md:text-4xl">404 - Page Not Found</h1>
-        <p className="font-matter max-w-2xl">
-          The page you are looking for might have been removed, had its name
-          changed, or is temporarily unavailable.
-        </p>
+      <Image
+        src="/404_bg.png"
+        alt="404 - Not Found"
+        fill
+        className="-z-20 object-cover"
+      />
 
-        <Link
+      <div className="absolute inset-0 -z-20 h-[30rem] w-full overflow-hidden">
+        <h2 className="mt-8 bg-[linear-gradient(180deg,_rgba(255,_255,_255,_0.06)_40.23%,_rgba(0,_0,_0,_0.01)_100%)] bg-clip-text text-center font-poppins text-[50vw] font-semibold uppercase text-transparent md:leading-none lg:text-[32vw]">
+          404
+        </h2>
+      </div>
+
+      <div className="flex flex-col items-center justify-center gap-y-6">
+        <div className="space-y-2 text-center">
+          <h1 className="font-poppins text-4xl font-bold md:text-5xl">
+            Seems like we&apos;ve lost you. <br />
+          </h1>
+
+          <p>For nerds, 404</p>
+        </div>
+
+        <Button
           href="/"
-          className="inline-flex text-blue-500 underline-offset-4 transition hover:text-blue-600 hover:underline"
+          round
+          className="mx-auto px-4 text-base shadow-[0px_4px_14.9px_0px_#986001]"
         >
-          <MoveLeft className="mr-2" />
-          Go back to homepage
-        </Link>
+          Go Home
+        </Button>
       </div>
     </div>
   );
