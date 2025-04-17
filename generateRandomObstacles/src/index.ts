f8e1df201316571c8ae7e0cf5ad11b5038b8ab7f
logcho <lsjcwma@gmail.com>
2025-04-17 00:29:13 -0500

    refactor: utilize builtin generate random function instead of lcg

--- generateRandomObstacles/src/index.ts
+++ generateRandomObstacles/src/index.ts
@@ -1,32 +1,13 @@
 /* eslint-disable no-restricted-properties */
 import { createApp } from "@deroll/app";
 import { createWallet } from "@deroll/wallet";
-import {
-  Address,
-  getAddress,
-  Hex,
-  hexToNumber,
-  hexToString,
-  toHex,
-} from "viem";
+import { getAddress, hexToString, toHex } from "viem";
 
 const app = createApp({
   url: process.env.ROLLUP_HTTP_SERVER_URL || "http://127.0.0.1:5004",
 });
 
 const wallet = createWallet();
-
-// Constants for LCG (can be customized as needed)
-const a = 1664525;
-const c = 1013904223;
-const m = Math.pow(2, 32);
-
-// Linear Congruential Generator (LCG) function
-function lcg(seed: number): number {
-  return (a * seed + c) % m;
-}
-
-let attempts: number = 0;
 
 app.addAdvanceHandler(wallet.handler);
 
@@ -39,21 +20,11 @@ function generateRandomObstacleDataFromHex(
   min: number,
   max: number
 ): number[][] {
-  // Convert the hex payload to a numeric seed and factor in the number of attempts
-
-  let seed = attempts;
-
-  // console.log("attempt:",attempts);
-
   // Create the 2D array of random obstacle data
   const random2DArray: number[][] = Array.from({ length: rows }, () => {
     const row = new Array(cols).fill(0); // Initialize a row of zeros
-    seed = lcg(seed); // Generate the next random number for the index
-    const randomIndex = seed % cols; // Random index for the non-zero element
-
-    seed = lcg(seed); // Generate the next random number for the value
-    const randomValue = (seed % (max - min + 1)) + min; // Random value between min and max
-
+    const randomIndex = Math.floor(Math.random() * cols); // Random index for the non-zero element
+    const randomValue = Math.floor(Math.random() * (max - min + 1)) + min; // Random value between min and max
     row[randomIndex] = randomValue; // Place the random value in the random index
     return row;
   });
@@ -75,7 +46,6 @@ app.addAdvanceHandler(async ({ metadata, payload }) => {
 
   if (jsonPayload.method === "generate_random") {
     // Increment the number of attempts before generating the random obstacle data
-    attempts += 1;
 
     // Generate a 2D array of random obstacle data using hex payload and attempts
     const randomObstacleData = generateRandomObstacleDataFromHex(
